bc531f6f8bbcb34d3e1e5c493d4a00940b804973
Brian Cao <98575161+CaoBeee@users.noreply.github.com>
2023-03-15 18:49:46 -0700

    repeatString

---
+++ freecodecamp/repeatAString.js
@@ -0,0 +1,21 @@
+/*
+Repeat a String Repeat a String
+Repeat a given string str (first argument) for num times (second argument). 
+Return an empty string if num is not a positive number. 
+For the purpose of this challenge, do not use the built-in .repeat() method.
+*/
+
+function repeatStringNumTimes(str, num) {
+	let result = str
+	if (num < 1) {
+		return ''
+	} else {
+		for (let i = 1; i < num; i++) {
+			result += str
+			console.log(str)
+		}
+	}
+	return result
+}
+
+repeatStringNumTimes('abc', 3)
