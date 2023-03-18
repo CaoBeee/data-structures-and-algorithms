9389a9fe36c0f6f69077fb0b88b9e82a4780b08d
Brian Cao <98575161+CaoBeee@users.noreply.github.com>
2023-03-18 11:43:43 -0700

    title case a sentence

--- freecodecamp/basic/titleCaseASentence.js
+++ freecodecamp/basic/titleCaseASentence.js
@@ -5,3 +5,15 @@ Make sure the rest of the word is in lower case.
 
 For the purpose of this exercise, you should also capitalize connecting words like the and of.
  */
+
+function titleCase(str) {
+	let sentence = ''
+	let stringArray = str.split(' ')
+	for (let i = 0; i < stringArray.length; i++) {
+		let word = stringArray[i]
+		sentence += word[0].toUpperCase() + word.substr(1, word.length).toLowerCase() + ' '
+	}
+	return sentence.trim()
+}
+
+titleCase("I'm a little tea pot")
